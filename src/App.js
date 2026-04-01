f20c0aa94ebde376850d4507b1a3e7cd83c6603c
giovannidevazevedo <giovanniazevedo065@gmail.com>
2026-04-01 09:58:04 -0300

    iniciando repositorio do projeto

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Header from './componetes/header/index.js'
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
+  
+
+
+`
+
+function App() {
+  return (
+      <AppContainer>
+        <Header/>
+      </AppContainer>
+        
+    
+  );
+}
+
+export default App;
